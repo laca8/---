6ea9b446ad3332b0861869adf78d5465181c00f6
laca8 <ahmedragab01140331005@gmail.com>
2025-01-18 14:50:00 +0200

    last update

--- clients/src/redux/service/notesReportService.jsx
+++ clients/src/redux/service/notesReportService.jsx
@@ -1,19 +1,18 @@
-import axios from 'axios'
-const API_URL = 'http://localhost:5000/api/note'
+import axios from "axios";
+const API_URL = "/api/note";
 
-const getNotes = async(id)=>{
-    const response = await axios.get(`${API_URL}/${id}`)
-    return await response.data
-}
+const getNotes = async (id) => {
+  const response = await axios.get(`${API_URL}/${id}`);
+  return await response.data;
+};
 
-const ctreatNote = async(n)=>{
-    const response = await axios.post(`${API_URL}`,n)
-    return await response.data
-}
-
+const ctreatNote = async (n) => {
+  const response = await axios.post(`${API_URL}`, n);
+  return await response.data;
+};
 
 const notesReportService = {
-       getNotes,
-       ctreatNote
-}
-export default notesReportService
+  getNotes,
+  ctreatNote,
+};
+export default notesReportService;
--- clients/vite.config.js
+++ clients/vite.config.js
@@ -4,6 +4,11 @@ import tailwindcss from "tailwindcss";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": "http://localhost:5000/",
+    },
+  },
   css: {
     postcss: {
       plugins: [tailwindcss()],
