70a5192838a92346d6303e6cec9773db224a0bb0
laca8 <ahmedragab01140331005@gmail.com>
2025-01-18 12:11:09 +0200

    last update

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const connDb = async () => {
   try {
-    await mongoose.connect("mongodb+srv://laca:jae09908@cluster0.gjxhg.mongodb.net/disables");
+    await mongoose.connect(
+      "mongodb+srv://laca:jae09908@cluster0.gjxhg.mongodb.net/disables"
+    );
     console.log("db connected...");
   } catch (err) {
-    console.log(err);
+    console.log(`mongoError${err}`);
   }
 };
 
