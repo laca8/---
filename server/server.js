802ceec64b9b5b635bb807a2150dd846d83307d6
laca8 <ahmedragab01140331005@gmail.com>
2025-01-18 15:46:49 +0200

    last update

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.use("*", (req, res, next) => {
 });
 app.use(globalError);
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET, POST, OPTIONS, PUT, PATCH, DELETE"
